eb922dc578e1539af095ef72df4ae10c1fd69d13
Bryan@iii <chunwei.liu@gmail.com>
2014-06-04 15:04:16 +0800

    寫sum.c 計算 1+2+3...+100 輸出 5050 另外一隻printNum.c 接收 並且列印"總數為:5050" 使用 ./sum | ./printNum

---
+++ printNum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	int sum;
+
+	scanf("%d",&sum);
+
+	printf("總數為：%d",sum);
+
+	return 0;
+}
---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	int sum=0;	
+	int i;
+	for(i=1;i<=100;i++){
+	sum += i;
+
+	}	
+
+	printf("%d",sum);
+
+	return 0;
+}
