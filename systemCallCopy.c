74b4d9221679e15db101119256e0fce62d772f40
Bryan@iii <chunwei.liu@gmail.com>
2014-06-04 10:45:26 +0800

    this is first file for read write files in linux

---
+++ systemCallCopy.c
@@ -0,0 +1,23 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+int main(void){
+	char c; // buffer
+	int in,out;
+
+	in = open("file.in",O_RDONLY);
+
+	out = open("file.out",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+	
+	while(read(in,&c,1)==1){
+	    write(out,&c,1);
+
+	}
+
+	close(in);
+	close(out);
+	return 0;
+
+
+}
+
