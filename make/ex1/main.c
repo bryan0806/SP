92ae4c2a3eea7e5514e588b92d05f4c48cb156e0
Bryan@iii <chunwei.liu@gmail.com>
2014-06-06 14:56:36 +0800

    still fail to make .... just don't know wht

--- make/ex1/main.c
+++ make/ex1/main.c
@@ -2,12 +2,10 @@
 // 這是第一個範例 來說明使用 makefile 讓指令make 了解如何編譯
 #include <stdlib.h>
 #include "a.h"
-
 extern void function_two();
 extern void function_three();
-int main(){
+int main(void){
 	function_two();
 	function_three();
 	exit(EXIT_SUCCESS);
-
 }
