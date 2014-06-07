c4fe457358bc3e92484b8fef5e8d123ac4d1e93a
bryan0806 <chunwei.liu@gmail.com>
2014-06-07 11:49:03 +0800

    Adding command discription

--- shell/sh08.sh
+++ shell/sh08.sh
@@ -1,4 +1,5 @@
 #sh08.sh
+#use command "sh sh08.sh one two three four five six" to see the result
 echo "Total parameter number is ==> $#"
 echo "Your whole parameter is ==> $@"
 shift #進行第一次 "一個變數的shift"
