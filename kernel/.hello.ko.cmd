db8b3893dab6a8f6fd33d7523a2cc0894918d519
Bryan@iii <chunwei.liu@gmail.com>
2014-06-09 10:47:35 +0800

    make 產生的檔案

---
+++ kernel/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/mnt/hgfs/share/SP/kernel/hello.ko := ld -m elf_i386 -r -m elf_i386  --build-id -o /mnt/hgfs/share/SP/kernel/hello.ko /mnt/hgfs/share/SP/kernel/hello.o /mnt/hgfs/share/SP/kernel/hello.mod.o
---
+++ kernel/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/mnt/hgfs/share/SP/kernel/hello.ko
+/mnt/hgfs/share/SP/kernel/hello.o
