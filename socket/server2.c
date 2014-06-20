d542d2b8afebc297455958535c635ddca295c9e4
bryan0806 <chunwei.liu@gmail.com>
2014-06-20 13:52:30 +0800

    add some commit for block situation and position

--- socket/server2.c
+++ socket/server2.c
@@ -16,12 +16,14 @@ int main(void){
 
 	/* Create an unnamed socket for the server */
 	server_sockfd=socket(AF_INET,SOCK_STREAM,0);
+	
 	/* Name the socket */
 	server_address.sin_family=AF_INET;
 	server_address.sin_addr.s_addr=inet_addr("127.0.0.1"); // 寫上server ip
 	server_address.sin_port=9734;
 	server_len=sizeof(server_address);
     bind(server_sockfd,(struct sockaddr *)&server_address,server_len);
+	
 	/* Create a connection queue and wait for clients */
 	listen(server_sockfd,5);
 	while(1){
@@ -30,6 +32,7 @@ int main(void){
 	/*Accept a connection */
 		client_len = sizeof(client_address);
 		client_sockfd=accept(server_sockfd,(struct sockaddr *)&client_address,&client_len);// server端接到client的connect之後產生一個connect socket(client socket) 然後以此cleint socket 作讀寫動作
+	
 	/* We can now read/write to client on client_sockfd */
     read(client_sockfd,&ch,1);
 	ch++;
